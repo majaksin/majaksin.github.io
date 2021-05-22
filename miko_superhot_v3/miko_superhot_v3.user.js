2a503cd48c213d10b5e5c4dae1c8cf5d60d73a2f
majaksin <67994038+majaksin@users.noreply.github.com>
2021-05-22 13:46:14 +0900

    Update and rename summer_superhot_v3.user.js to miko_superhot_v3.user.js

--- miko_superhot_v3/miko_superhot_v3.user.js
+++ miko_superhot_v3/miko_superhot_v3.user.js
@@ -1,26 +1,26 @@
 // ==UserScript==
 // @name         한여름의 소녀들 (R-18)
-// @namespace    summer_superhot_v3
+// @namespace    miko_superhot_v3
 // @version      2.1.8
 // @description  마이와 이치히메, 지니아가 수영복을 모두 탈의합니다.
 // @author       ㅇㅇ
 // @license      MIT
-// @icon         https://majaksin.github.io/summer_superhot_v3/preview.png
-// @supportURL   https://github.com/majaksin/summer_superhot_v3/issues
-// @homepageURL  https://github.com/majaksin/summer_superhot_v3
-// @downloadURL  https://majaksin.github.io/summer_superhot_v3/summer_superhot_v3.user.js
-// @updateURL    https://majaksin.github.io/summer_superhot_v3/summer_superhot_v3.user.js
+// @icon         https://majaksin.github.io/miko_superhot_v3/preview.png
+// @supportURL   https://github.com/majaksin/miko_superhot_v3/issues
+// @homepageURL  https://github.com/majaksin/miko_superhot_v3
+// @downloadURL  https://majaksin.github.io/miko_superhot_v3/miko_superhot_v3.user.js
+// @updateURL    https://majaksin.github.io/miko_superhot_v3/miko_superhot_v3.user.js
 // @include      https://game.mahjongsoul.com/*
 // @grant        unsafeWindow
 // @grant        GM_getResourceText
 // @run-at       document-start
-// @resource resourcepack https://majaksin.github.io/summer_superhot_v3/resourcepack.json
+// @resource resourcepack https://majaksin.github.io/miko_superhot_v3/resourcepack.json
 // ==/UserScript==
 
 (function () {
     'use strict';
     const GAME_BASE_URL = 'https://game.mahjongsoul.com/';
-    const RES_BASE_URL = 'https://majaksin.github.io/summer_superhot_v3/';
+    const RES_BASE_URL = 'https://majaksin.github.io/miko_superhot_v3/';
 
     const version_re = /v\d+\.\d+\.\d+\.w\//i;
     const resourcepack = JSON.parse(GM_getResourceText('resourcepack'));
